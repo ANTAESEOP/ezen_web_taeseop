dc4c96302f4e4b7e45946675f354121d1c1e463f
504 <504@DESKTOP-O6PNQCD>
2022-08-30 17:06:48 +0900

    asd

--- frontq/public/CSS/웹디자인/챕터4/script.js
+++ frontq/public/CSS/웹디자인/챕터4/script.js
@@ -1,3 +1,5 @@
+
+let 지원자리스트 =[]
 /* chartist.js 이용한 차트 구현 */
 /* 
 	변수 = 값 
@@ -66,4 +68,30 @@ new Chartist.Bar('.bar_chart' , barData , barOptions )
 	barData2.series[0].push( money )
 	// 차트 작성
 new Chartist.Bar('.bar_chart2' , barData2 , barOptions2 )
+}
+
+
+// 1.
+function 지원자등록(){
+	console.log('지원자 함수 실행')
+	let 입력리스트 = document.querySelectorAll('input')
+	let 목록상자 = document.querySelector('select')
+	let 긴글상자 = document.querySelector('textarea')
+	//2번 테스트
+	console.log(입력리스트)
+	console.log(목록상자)
+	console.log(긴글상자)
+
+let 지원자 = {
+	성명 : 입력리스트[0].value,
+	영문명 : 입력리스트[1].value,
+	메일주소 : 입력리스트[2].value,
+	포토폴리오 : 입력리스트[3].value,
+	희망근무지 : 입력리스트[4].value,
+	현재상태 : 목록상자.value ,
+	자기소개 : 긴글상자.value , 
+}
+console.log(지원자)
+지원자리스트.push (지원자)
+console.log(지원자리스트)
 }
