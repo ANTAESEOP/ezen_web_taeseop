a2cc64337086172a0f2b2a10e8e592801f1ce9a9
504 <504@DESKTOP-O6PNQCD>
2022-08-31 15:40:35 +0900

    2022-08-31 / 15 : 40

---
+++ frontq/취합/회원가입.js
@@ -0,0 +1,46 @@
+let 전화번호=''
+function 가입하기(){
+	let 입력=''
+	alert('가입이 완료되었습니다')
+	 location.href = "로그인.html";
+	
+}
+function 회원가입(){
+	let result = confirm('회원가입페이지로 넘어가겠습니까?')
+	if(result == true){
+		alert ('회원가입페이지로이동합니다.')
+		location.href = "회원가입.html";
+	}else{
+		alert('이전페이지로이동합니다')
+		}
+	
+}
+
+document.getElementById('buttom').innerHTML = value
+
+/*function 번호입력(){
+	let 전화번호 = ''
+	for(let i='';){
+	if(전화번호[i]=Number){
+		alert('번호로입력해주세요')
+	}document.querySelector('.mam')= value
+	}
+}*/
+function 인증받기(){
+	if(인증받기!=확인){
+	alert('인증번호를 보냈습니다.')
+	}else{
+		alert('인증해주세요')
+	}
+}
+document.getElementById('cols2').innerHTML = value
+
+function 확인(){
+	alert('인증되었습니다.')
+}	
+document.getElementById('cols2').innerHTML = value
+
+function 로그인(){
+	alert(`${i}+님 안녕하세요`)
+}
+document.getElementById('login2').innerHTML = value
