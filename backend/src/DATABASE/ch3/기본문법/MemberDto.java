d7fad6fce54962fc2b12e4a5c75534e7cd4c0df1
태섭 <@118.36.71.126>
2022-09-25 23:47:50 +0900

    과제완

--- backend/src/DATABASE/ch3/기본문법/MemberDto.java
+++ backend/src/DATABASE/ch3/기본문법/MemberDto.java
@@ -11,12 +11,19 @@ public class MemberDto {
 	String phone2;					
 	short height;			
 	String debut_date;
+	int num;
+	String pord_name;
+	String group_name;
+	int price;
+	int amount;
+	
 	
 	// 2. 생성자
 	public MemberDto() {}
-	
+
+
 	public MemberDto(String mem_id, String mem_name, int mem_number, String addr, String phone1, String phone2,
-			short height, String debut_date) {
+			short height, String debut_date, int num, String pord_name, String group_name, int price, int amount) {
 		super();
 		this.mem_id = mem_id;
 		this.mem_name = mem_name;
@@ -26,5 +33,13 @@ public class MemberDto {
 		this.phone2 = phone2;
 		this.height = height;
 		this.debut_date = debut_date;
-	}		
+		this.num = num;
+		this.pord_name = pord_name;
+		this.group_name = group_name;
+		this.price = price;
+		this.amount = amount;
+	}
+	
+	
+		
 }
